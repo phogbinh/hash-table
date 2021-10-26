6fa3ee1c259fd345008c887161ba5c9cdfa41f3f
bill-msi-laptop <bill-msi-laptop>
2021-10-26 13:24:04 +0800

    [master] Added test that shows bug of my code.

--- HashTable/main.cpp
+++ HashTable/main.cpp
@@ -7,8 +7,10 @@ int main()
   hashMap.observe("Vagina");
   hashMap.observe("Potato");
   hashMap.observe("Vagina");
+  hashMap.observe("anigaV");
   printf("%d\n", hashMap.getCount("Vagina")); // 2
   printf("%d\n", hashMap.getCount("Potato")); // 1
   printf("%d\n", hashMap.getCount("Hello")); // 0
+  printf("%d\n", hashMap.getCount("anigaV")); // 1
   return 0;
 }
